21c395c1850ac0e0119e8a0575a52ec906efa463
jeremine-doan <122441897+jeremine-doan@users.noreply.github.com>
2024-06-21 19:57:33 +0700

    Update EXC_01_top_sales_representatives.sql

--- EXC_01_top_sales_representatives.sql
+++ EXC_01_top_sales_representatives.sql
@@ -3,26 +3,26 @@ Write a query to return Top 5 sales representatives in 2003.
 */
 
 SELECT
-	e.employeeNumber,
-	CONCAT_WS(" ", e.firstName, e.lastName) AS employeeName,
-	SUM(d.quantityOrdered * d.priceEach) AS revenueIn2003
+    e.employeeNumber,
+    CONCAT_WS(" ", e.firstName, e.lastName) AS employeeName,
+    SUM(d.quantityOrdered * d.priceEach) AS revenueIn2003
 FROM
-	employees AS e
+    employees AS e
 LEFT JOIN
-	customers AS c
-	ON c.salesRepEmployeeNumber = e.employeeNumber
+    customers AS c
+    ON c.salesRepEmployeeNumber = e.employeeNumber
 INNER JOIN
-	orders AS o
-	ON o.customerNumber = c.customerNumber
+    orders AS o
+    ON o.customerNumber = c.customerNumber
 INNER JOIN
-	orderdetails AS d
-	ON d.orderNumber = o.orderNumber
+    orderdetails AS d
+    ON d.orderNumber = o.orderNumber
 WHERE
-	YEAR(o.orderDate) = 2003
+    YEAR(o.orderDate) = 2003
 GROUP BY
-	e.employeeNumber,
-	CONCAT_WS(" ", e.firstName, e.lastName)
+    e.employeeNumber,
+    CONCAT_WS(" ", e.firstName, e.lastName)
 ORDER BY
-	SUM(d.quantityOrdered * d.priceEach) DESC
+    SUM(d.quantityOrdered * d.priceEach) DESC
 LIMIT
-	5;
+    5;
