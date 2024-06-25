4ba77bb892af3433868ddc5405826938007067cd
jeremine-doan <122441897+jeremine-doan@users.noreply.github.com>
2024-06-25 15:38:56 +0700

    Update code 05 - Employee hierarchy.sql

--- code 05 - Employee hierarchy.sql
+++ code 05 - Employee hierarchy.sql
@@ -3,32 +3,32 @@ Write a query to return employees' level in the company hierarchy.
 */
 
 WITH RECURSIVE cte AS (
-	SELECT
-		e.employeeNumber,
-		CONCAT_WS(" ", e.firstName, e.lastName) AS employeeName,
-		e.reportsTo,
-		0 AS hierarchy
-	FROM
-		employees AS e
-	WHERE
-		e.reportsTo IS NULL
-	UNION ALL
-	SELECT
-		e.employeeNumber,
-		CONCAT_WS(" ", e.firstName, e.lastName) AS employeeName,
-		e.reportsTo,
-		cte.hierarchy + 1 AS hierarchy
-	FROM
-		employees AS e
-	INNER JOIN
-		cte
-		ON e.reportsTo = cte.employeeNumber
+    SELECT
+        e.employeeNumber,
+        CONCAT_WS(" ", e.firstName, e.lastName) AS employeeName,
+        e.reportsTo,
+        0 AS hierarchy
+    FROM
+        employees AS e
+    WHERE
+        e.reportsTo IS NULL
+    UNION ALL
+    SELECT
+        e.employeeNumber,
+        CONCAT_WS(" ", e.firstName, e.lastName) AS employeeName,
+        e.reportsTo,
+        cte.hierarchy + 1 AS hierarchy
+    FROM
+        employees AS e
+    INNER JOIN
+        cte
+        ON e.reportsTo = cte.employeeNumber
 )
 SELECT
-	*
+    *
 FROM
-	cte
+    cte
 ORDER BY
-	hierarchy,
+    hierarchy,
     reportsTo,
     employeeNumber;
