cfbdba8e975e6aa3f3a2816a9170ac2b3bcd7712
jeremine-doan <122441897+jeremine-doan@users.noreply.github.com>
2024-06-25 15:33:58 +0700

    Update code 03 - Revenue by product line.sql

--- code 03 - Revenue by product line.sql
+++ code 03 - Revenue by product line.sql
@@ -6,32 +6,32 @@ Write a query to return a result set which includes:
 */
 
 WITH productLineRevenue AS (
-	SELECT
-		p.productLine,
-		YEAR(o.orderDate) AS orderYear,
-		SUM(d.quantityOrdered * d.priceEach) AS revenue,
-		GROUPING(p.productLine) AS all_productLines,
-		GROUPING(YEAR(o.orderDate)) AS all_years
-	FROM
-		products AS p
-	INNER JOIN
-		orderdetails AS d
-		USING(productCode)
-	INNER JOIN
-		orders AS o
-		USING(orderNumber)
-	GROUP BY
-		p.productLine,
-		YEAR(o.orderDate)
-	WITH ROLLUP
+    SELECT
+        p.productLine,
+        YEAR(o.orderDate) AS orderYear,
+        SUM(d.quantityOrdered * d.priceEach) AS revenue,
+        GROUPING(p.productLine) AS all_productLines,
+        GROUPING(YEAR(o.orderDate)) AS all_years
+    FROM
+        products AS p
+    INNER JOIN
+        orderdetails AS d
+        USING(productCode)
+    INNER JOIN
+        orders AS o
+        USING(orderNumber)
+    GROUP BY
+        p.productLine,
+        YEAR(o.orderDate)
+    WITH ROLLUP
 )
 SELECT
-	IF(all_productLines, "_TOTAL_", productLine) AS productLine,
+    IF(all_productLines, "_TOTAL_", productLine) AS productLine,
     CASE
-		WHEN all_years AND all_productLines THEN ""
-		WHEN all_years THEN "_all_years_"
+        WHEN all_years AND all_productLines THEN ""
+        WHEN all_years THEN "_all_years_"
         ELSE orderYear
     END AS orderYear,
     revenue
 FROM
-	productLineRevenue;
+    productLineRevenue;
